0a95b82cf1cba2498f1127e903ebce64d73e5681
Aleksey Zholobenko <aleshaleksey@googlemail.com>
2022-05-26 21:26:07 +0200

    Now with `cargo fmt --all && cargo clippy --all`.

--- build.rs
+++ build.rs
@@ -5,10 +5,9 @@ use std::path::PathBuf;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let path = PathBuf::from(&crate_dir).join("cbindgen.toml");
-    let config = cbindgen::Config::from_file(path)
-        .expect("Config parsing failed, because");
+    let config = cbindgen::Config::from_file(path).expect("Config parsing failed, because");
 
     cbindgen::generate_with_config(crate_dir, config)
-      .expect("Unable to generate bindings")
-      .write_to_file("include/libazdice.h");
+        .expect("Unable to generate bindings")
+        .write_to_file("include/libazdice.h");
 }
